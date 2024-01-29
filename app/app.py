3e37680fb767ad4139731d2b70f1cebbf8c0cc58
Filip Woźniak <s22703@pjwstk.edu.pl>
2024-01-29 14:00:45 +0100

    updated app.py

--- app/app.py
+++ app/app.py
@@ -16,20 +16,36 @@ def load_model():
     with open('model.h5', 'rb') as file:
         model = pickle.load(file)
     return model
-
+# kolumny
+def load_model_columns():
+    with open('model_columns.pkl', 'rb') as file:
+        columns = pickle.load(file)
+    return columns
 # funkcja do przetwarzania danych wejsciowych
-def process_input(data, encoder):
-    # kodowanie binarne dla rain today
-    data = encoder.transform(data)
+def process_input(data, encoder, features = load_model_columns()):
 
-    # zmienne dummy
-    data = pd.get_dummies(data, columns=['Location', 'WindGustDir', 'WindDir9am', 'WindDir3pm', 'Coordinates'])
+    data_binary_encoded = encoder.transform(data[['RainToday']])
 
-    # skalowanie
+    
+    categorical_columns = ['Location', 'WindGustDir', 'WindDir9am', 'WindDir3pm']
+    data_one_hot_encoded = pd.get_dummies(data[categorical_columns])
+
+    
+    data_combined = pd.concat([data.drop(['RainToday'] + categorical_columns, axis=1),
+                               data_binary_encoded, data_one_hot_encoded], axis=1)
+
+    
+    for col in features:
+        if col not in data_combined.columns:
+            data_combined[col] = 0
+    data_prepared = data_combined.reindex(columns=features)
+
+    
     scaler = MinMaxScaler()
-    data = scaler.fit_transform(data)
+    data_scaled = scaler.fit_transform(data_prepared)
+    print(data_scaled)
+    return data_scaled
 
-    return data
 
 def main():
     st.set_page_config(page_title="Czy jutro będzie padać?")
@@ -46,35 +62,38 @@ def main():
         humidity_3pm = st.number_input('Humidity3pm', value=50)
         pressure_9am = st.number_input('Pressure9am', value=1010.0)
         pressure_3pm = st.number_input('Pressure3pm', value=1010.0)
+        latitude = st.number_input('Latitude', value=-37.81)
+        longtitude = st.number_input('Longtitude', value=144.97)
         year = st.number_input('Year', min_value=2000, max_value=2020, value=2010)
         month = st.number_input('Month', min_value=1, max_value=12, value=6)
         day = st.number_input('Day', min_value=1, max_value=31, value=15)
         rain_today = st.selectbox('RainToday', ['Yes', 'No'])
-        location = st.selectbox('Location', ['Location1', 'Location2', 'Location3']) # to tylko dla testu
+        location = st.selectbox('Location', 
+        ['Adelaide', 'Albany', 'Albury', 'AliceSprings', 'Ballarat', 'Bendigo', 'Brisbane', 'Cairns', 'Canberra',
+         'Darwin','GoldCoast', 'Hobart', 'Launceston', 'Melbourne', 'MountGambier', 'MountGinini', 'Newcastle', 'Penrith',
+         'Perth','Sydney', 'Townsville', 'Tuggeranong', 'Wollongong'
+        ])
+        
         wind_gust_dir = st.selectbox('WindGustDir', ['N', 'S', 'E', 'W'])
         wind_dir_9am = st.selectbox('WindDir9am', ['N', 'S', 'E', 'W'])  
         wind_dir_3pm = st.selectbox('WindDir3pm', ['N', 'S', 'E', 'W'])  
-        coordinates = st.selectbox('Coordinates', ['Coord1', 'Coord2', 'Coord3'])  
 
         submit_button = st.form_submit_button(label='Predict')
 
-    if submit_button:
-        # przygtowanie danych wejsciowych
-        input_data = pd.DataFrame([[min_temp, max_temp, rainfall, wind_gust_speed, wind_speed_9am, wind_speed_3pm,
-                                    humidity_3pm, pressure_9am, pressure_3pm, year, month, day, rain_today,
-                                    location, wind_gust_dir, wind_dir_9am, wind_dir_3pm, coordinates]],
-                                  columns=['MinTemp', 'MaxTemp', 'Rainfall', 'WindGustSpeed', 'WindSpeed9am', 'WindSpeed3pm',
-                                           'Humidity3pm', 'Pressure9am', 'Pressure3pm', 'Year', 'Month', 'Day', 'RainToday',
-                                           'Location', 'WindGustDir', 'WindDir9am', 'WindDir3pm', 'Coordinates'])
+        model_columns = load_model_columns() 
         
-        encoder = load_encoder()
-        # dane wejściowe
-        processed_data = process_input(input_data, encoder)
 
-        # model
+    if submit_button:
+        # Prepare input data
+        input_data = pd.DataFrame([[min_temp, max_temp, rainfall, wind_gust_speed, wind_speed_9am, wind_speed_3pm,
+                                    humidity_3pm, pressure_9am, pressure_3pm, latitude, longtitude, year, month, day, rain_today,
+                                    location, wind_gust_dir, wind_dir_9am, wind_dir_3pm]],
+                                  columns=['MinTemp', 'MaxTemp', 'Rainfall', 'WindGustSpeed', 'WindSpeed9am', 'WindSpeed3pm',
+                                           'Humidity3pm', 'Pressure9am', 'Pressure3pm', 'Latitude', 'Longitude', 'Year', 'Month', 'Day', 'RainToday',
+                                           'Location', 'WindGustDir', 'WindDir9am', 'WindDir3pm'])
+        # Model prediction
+        processed_data = process_input(input_data, encoder, model_columns)
         model = load_model()
-
-        # predykcja
         prediction = model.predict(processed_data)
         st.write(f'Prediction: {"Rain" if prediction[0] == "Yes" else "No Rain"}')
 
